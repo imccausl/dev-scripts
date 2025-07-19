7e7421012cda9a4652a6b14db0793f7c6fd496c5
Ian McCausland <imccausl@gmail.com>
2025-07-19 13:54:34 -0700

    wip: disable require for prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const prettierConfig = require('./src/format/config.default.cjs')
 
 module.exports = {
-  ...prettierConfig,
+    ...prettierConfig,
 }
