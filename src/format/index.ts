2f9171892342282a384b25d28fa08e961aa85bf0
Ian McCausland <imccausl@gmail.com>
2025-10-26 17:45:14 -0400

    fix: add release-it

--- src/format/index.ts
+++ src/format/index.ts
@@ -14,8 +14,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 function hereRelative(p: string) {
-  console.log(`Resolving path relative to: ${__dirname}`)
-  console.log(`full path: ${here(p, __dirname).replace(process.cwd(), '.')}`)
+  if (process.env.DEBUG === '1'){
+    console.log(`Resolving path relative to: ${__dirname}`)
+    console.log(`full path: ${here(p, __dirname).replace(process.cwd(), '.')}`)
+  }
   return here(p, __dirname).replace(process.cwd(), '.')
 }
 
